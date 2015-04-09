f7c85625cf579a28caba4492167a825817e7c3f8
colosicm <colosicm@gmail.com>
2015-04-09 00:21:48 -0500

    OrderBook and Trader hopefully fully implemented

--- src/pkg/order/OrderBook.java
+++ src/pkg/order/OrderBook.java
@@ -2,9 +2,9 @@ package pkg.order;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.TreeMap;
 
+import pkg.exception.StockMarketExpection;
 import pkg.market.Market;
 import pkg.market.api.PriceSetter;
 
@@ -13,18 +13,11 @@ public class OrderBook {
 	PriceSetter ps;
 	HashMap<String, ArrayList<Order>> buyOrders;
 	HashMap<String, ArrayList<Order>> sellOrders;
-	HashMap<Double, Integer> cumulativeBuyOrders; //price, total order volume for price
-	HashMap<Double, Integer> cumulativeSellOrders; //price, total order volume for price
-
-	int[] cumulativeBuyPerPrice;
-	int[] cumulativeSellPerPrice;
 
 	public OrderBook(Market m) {
 		this.m = m;
 		buyOrders = new HashMap<String, ArrayList<Order>>();
 		sellOrders = new HashMap<String, ArrayList<Order>>();
-		cumulativeBuyOrders = new HashMap<Double, Integer>();
-		cumulativeSellOrders = new HashMap<Double, Integer>();
 	}
 
 	public void addToOrderBook(Order order) {
@@ -60,28 +53,7 @@ public class OrderBook {
 	public void trade() {
 		// Complete the trading.
 		// 1. Follow and create the orderbook data representation (see spec)
-		Iterator<Map.Entry<String, ArrayList<Order>>> it = sellOrders.entrySet().iterator();
-		double matchPrice = 0.0;
-		while (it.hasNext()) {
-			Map.Entry<String, ArrayList<Order>> pair = (Map.Entry<String, ArrayList<Order>>)it.next();
-			for (Order o : pair.getValue()) {
-				updateOrderBook(o);
-			}
-			
-			for (int i = 0; i < cumulativeSellOrders.keySet().size(); i++) {
-				double nextPrice;
-				//int next = cumulativeSellOrders.get();
-				
-			}
-			System.out.println(cumulativeSellOrders.toString());
 
-			matchPrice = findMatchPrice(matchPrice);
-			ps.setNewPrice(this.m, pair.getKey(), matchPrice);
-			//TODO observer pattern?
-			ps.notifyObserver();
-			//TODO steps 4 & 5
-			it.remove(); // avoids a ConcurrentModificationException
-		}
 		// 2. Find the matching price
 		// 3. Update the stocks price in the market using the PriceSetter.
 		// Note that PriceSetter follows the Observer pattern. Use the pattern.
@@ -90,43 +62,163 @@ public class OrderBook {
 		// trader's orders can be placed to his possession (a trader's position
 		// is the stocks he owns)
 		// (Add other methods as necessary)
-	}
+		for (String stock : sellOrders.keySet()) {
+			if (buyOrders.containsKey(stock)) {
+				ArrayList<Order> buying = buyOrders.get(stock);
+				ArrayList<Order> selling = sellOrders.get(stock);
+				ArrayList<Order> orders;
 
-	private double findMatchPrice(double matchPrice) {
-		//TODO find minimum of two values
-		//cumulative least favorable price
-		int tempMaxTrades = 0;
-		for (double price : cumulativeSellOrders.keySet()) {
-			int numTrades = Math.abs(cumulativeSellOrders.get(price) - cumulativeBuyOrders.get(price));
-			if (numTrades > tempMaxTrades) {
-				tempMaxTrades = numTrades;
-				matchPrice = price;
+				TreeMap<Double, ArrayList<Order>> sorted = new TreeMap<Double, ArrayList<Order>>();
+
+
+				// populate sorted with both buy and sell orders for each price
+				for (Order buyOrder : buying) {
+					// get arraylist of orders for price
+					if (sorted.containsKey(buyOrder.getPrice())) {
+						orders = sorted.get(buyOrder.getPrice());
+					} else { // if none exists, create a new arraylist
+						orders = new ArrayList<Order>();
+					}
+					orders.add(buyOrder);
+					sorted.put(buyOrder.getPrice(), orders);
+				}
+				for (Order sellOrder : selling) {
+					// get arraylist of orders for price
+					if (sorted.containsKey(sellOrder.getPrice())) {
+						orders = sorted.get(sellOrder.getPrice());
+					} else { // if none exists, create a new arraylist
+						orders = new ArrayList<Order>();
+					}
+					orders.add(sellOrder);
+					sorted.put(sellOrder.getPrice(), orders);
+				}
+
+				// handle and remove market orders
+				double marketPrice = m.getStockForSymbol(stock).getPrice();
+				ArrayList<Order> marketOrders = sorted.remove(0.0);
+				int runningBuyTotal = 0, runningSellTotal = 0;
+
+				for (Order marketOrder : marketOrders) {
+					if (marketOrder instanceof BuyOrder) {
+						runningBuyTotal += marketOrder.getSize();
+					} else {
+						runningSellTotal += marketOrder.getSize();
+					}
+				}
+
+				// construct cumulative least favorably price list
+				int numPrices = sorted.size();
+				int[] cumulativeBuysPerPrice = new int[numPrices];
+				int[] cumulativeSellsPerPrice = new int[numPrices];
+
+				// add up cumulative sell orders at each price
+				int i = 0;
+				for (double price : sorted.keySet()) {
+					ArrayList<Order> ordersAtPrice = sorted.get(price);
+
+					for (Order o : ordersAtPrice) {
+						if (o instanceof SellOrder) {
+							runningSellTotal += o.getSize();
+						}
+					}
+
+					cumulativeSellsPerPrice[i] = runningSellTotal;
+					i++;
+				}
+
+				// add up cumulative sell orders at each price
+				int j = numPrices - 1;
+				for (double price : sorted.descendingKeySet()) {
+					ArrayList<Order> ordersAtPrice = sorted.get(price);
+
+					for (Order o : ordersAtPrice) {
+						if (o instanceof BuyOrder) {
+							runningBuyTotal += o.getSize();
+						}
+					}
+
+					cumulativeBuysPerPrice[j] = runningBuyTotal;
+					j--;
+				}
+
+
+				// find matching price
+				int delta = Integer.MAX_VALUE;
+				int k = 0, matchingIndex = -1;
+				double matchingPrice = marketPrice;
+
+				while (delta > 0 && k < numPrices) {
+					int newDelta = cumulativeBuysPerPrice[k] - cumulativeSellsPerPrice[k];
+					if (newDelta < delta) {
+						delta = newDelta;
+						if (newDelta >= 0) {
+							matchingIndex = k;
+						}
+					}
+					k++;
+				}
+				for (double price : sorted.keySet()) {
+					if (matchingIndex == 0) {
+						matchingPrice = price;
+						break;
+					}
+					matchingIndex--;
+				}
+
+				PriceSetter priceSetter = new PriceSetter();
+				priceSetter.registerObserver(m.getMarketHistory());
+				m.getMarketHistory().setSubject(priceSetter);
+				if (matchingPrice != marketPrice) {
+					priceSetter.setNewPrice(m, stock, matchingPrice);
+				}
+
+				// remove traded orders from orderbook and delegate to trader
+				for (Order marketOrder : marketOrders) { // remove market trades
+					if (marketOrder instanceof BuyOrder) {
+						buyOrders.get(stock).remove(marketOrder);
+					} else {
+						sellOrders.get(stock).remove(marketOrder);
+					}
+
+					try { // delegate to trader
+						marketOrder.getTrader().tradePerformed(marketOrder, matchingPrice);
+					} catch (StockMarketExpection e) {
+						e.printStackTrace();
+					}
+				}
+
+				for (double price : sorted.keySet()) { // remove all other executed trades
+					ArrayList<Order> ordersAtPrice = sorted.get(price);
+
+					for (Order o : ordersAtPrice) {
+						if (o instanceof BuyOrder && price >= matchingPrice) {
+							buyOrders.get(stock).remove(o);
+
+							try { // delegate to trader
+								o.getTrader().tradePerformed(o, matchingPrice);
+							} catch (StockMarketExpection e) {
+								e.printStackTrace();
+							}
+						} else if (o instanceof SellOrder && price <= matchingPrice) {
+							sellOrders.get(stock).remove(o);
+
+							try { // delegate to trader
+								o.getTrader().tradePerformed(o, matchingPrice);
+							} catch (StockMarketExpection e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
 			}
 		}
-		return matchPrice;
 	}
-
-	private void updateOrderBook(Order o) {
-		if (o instanceof BuyOrder) {
-			if (cumulativeBuyOrders.containsKey(o.price)) {
-				int volume = cumulativeBuyOrders.get(o.price);
-				volume += o.size;
-				cumulativeBuyOrders.put(o.price, volume);
-			}
-			else {
-				cumulativeBuyOrders.put(o.price, o.size);
-			}
-		}
-		if (o instanceof SellOrder) {
-			if (cumulativeSellOrders.containsKey(o.price)) {
-				int volume = cumulativeSellOrders.get(o.price);
-				volume += o.size;
-				cumulativeSellOrders.put(o.price, volume);
-			}
-			else {
-				cumulativeSellOrders.put(o.price, o.size);
-			}
-		}
-	}
-
 }
+			
+		
+	
+
+
+
+
+
--- src/pkg/trader/Trader.java
+++ src/pkg/trader/Trader.java
@@ -32,7 +32,7 @@ public class Trader {
 			throws StockMarketExpection {
 
 		double price = m.getStockForSymbol(symbol).getPrice();
-		if (price > this.cashInHand) {
+		if ((price * volume) > this.cashInHand) {
 			throw new StockMarketExpection(this.name + " cannot afford to buy this stock");
 		}
 		Order order = new BuyOrder(symbol, volume, price , this);
@@ -63,83 +63,92 @@ public class Trader {
 		// own. Or he cannot sell more stocks than he possesses. Throw an
 		// exception in these cases.
 
-		if (price > cashInHand) {
-			throw new StockMarketExpection("Trader does not have enough cash");
+		double total = price * volume;
+
+		//Check if trader has enough cash to buy stock
+		if (orderType == OrderType.BUY && total > cashInHand) {
+			throw new StockMarketExpection(this.name + " cannot afford to buy this stock");
 		}
 
 		Order order = null;
-
 		if (orderType == OrderType.BUY) {
 			order = new BuyOrder(symbol, volume, price, this);
 		}
-		else if (orderType == OrderType.SELL) {
-			if (OrderUtility.owns(this.position, symbol)) {
-				if (OrderUtility.ownedQuantity(this.position, symbol) <= volume) {
-					order = new SellOrder(symbol, volume, price, this);
-				}
-				else {
-					throw new StockMarketExpection("Sell order volume is larger than amount of stock owned");
-				}
-			}
-			else {
-				throw new StockMarketExpection(this.name + " cannot place a sell order for a stock not owned");
-			}
+		else {
+			order = new SellOrder(symbol, volume, price, this);
 		}
 
+		//Check for multiple stock orders
+		if (OrderUtility.isAlreadyPresent(ordersPlaced, order)) {
+			throw new StockMarketExpection("There is already an order for this stock");
+		}
+
+		// Check that trader owns enough of stock being sold
+		if (orderType == OrderType.SELL && volume > OrderUtility.ownedQuantity(this.position, symbol)) {
+			throw new StockMarketExpection("Sell order volume is larger than amount of stock owned");
+		}
+
+		// Check that trader owns the stock
+		if (orderType == OrderType.SELL && !OrderUtility.owns(position, symbol)) {
+			throw new StockMarketExpection(this.name + " cannot place a sell order for a stock not owned");
+		}
+
+		// Check for invalid orders
 		if (order == null) {
 			throw new StockMarketExpection("Not a valid order type");
 		}
-		else {
-			if (OrderUtility.isAlreadyPresent(this.ordersPlaced, order)) {
-				throw new StockMarketExpection("There is already an order for this stock");
-			}
-			else {
-				this.ordersPlaced.add(order);
-				m.addOrder(order);
-			}
 
-		}
+		// Enter order into orderbook
+		m.addOrder(order);
+
+		//Update orders placed
+		this.ordersPlaced.add(order);
 	}
 
 	public void placeNewMarketOrder(Market m, String symbol, int volume,
 			double price, OrderType orderType) throws StockMarketExpection {
 		// Similar to the other method, except the order is a market order
-				if (price > cashInHand) {
-					throw new StockMarketExpection("Trader does not have enough cash");
-				}
+		double total = m.getStockForSymbol(symbol).getPrice() * volume;
 
-				Order order = null;
+		//Check if trader has enough cash to buy stock
+		if (total > cashInHand) {
+			throw new StockMarketExpection(this.name + " cannot afford to buy this stock");
+		}
 
-				if (orderType == OrderType.BUY) {
-					order = new BuyOrder(symbol, volume, true, this);
-				}
-				else if (orderType == OrderType.SELL) {
-					if (OrderUtility.owns(this.position, symbol)) {
-						if (OrderUtility.ownedQuantity(this.position, symbol) <= volume) {
-							order = new SellOrder(symbol, volume, true, this);
-						}
-						else {
-							throw new StockMarketExpection("Sell order volume is larger than amount of stock owned");
-						}
-					}
-					else {
-						throw new StockMarketExpection(this.name + " cannot place a sell order for a stock not owned");
-					}
-				}
+		Order order = null;
+		if (orderType == OrderType.BUY) {
+			order = new BuyOrder(symbol, volume, true, this);
+		}
+		else {
+			order = new SellOrder(symbol, volume, true, this);
+		}
 
-				if (order == null) {
-					throw new StockMarketExpection("Not a valid order type");
-				}
-				else {
-					if (OrderUtility.isAlreadyPresent(this.ordersPlaced, order)) {
-						throw new StockMarketExpection("There is already an order for this stock");
-					}
-					else {
-						this.ordersPlaced.add(order);
-						m.addOrder(order);
-					}
+		//Check for multiple stock orders
+		if (OrderUtility.isAlreadyPresent(ordersPlaced, order)) {
+			throw new StockMarketExpection("There is already an order for this stock");
+		}
 
-				}
+		// Check that trader owns enough of stock being sold
+		if (orderType == OrderType.SELL && volume > OrderUtility.ownedQuantity(this.position, symbol)) {
+			throw new StockMarketExpection("Sell order volume is larger than amount of stock owned");
+		}
+
+		// Check that trader owns the stock
+		if (orderType == OrderType.SELL && !OrderUtility.owns(position, symbol)) {
+			throw new StockMarketExpection(this.name + " cannot place a sell order for a stock not owned");
+		}
+
+		// Check for invalid orders
+		if (order == null) {
+			throw new StockMarketExpection("Not a valid order type");
+		}
+
+		// Enter order into orderbook
+		m.addOrder(order);
+
+		//Update orders placed
+		this.ordersPlaced.add(order);
+
 	}
 
 	public void tradePerformed(Order o, double matchPrice)
@@ -152,32 +161,24 @@ public class Trader {
 
 		// Update the trader's orderPlaced, position, and cashInHand members
 		// based on the notification.
-		Order order = OrderUtility.findAndExtractOrder(o.getTrader().position, o.getStockSymbol());
+		if (!OrderUtility.isAlreadyPresent(ordersPlaced, o)) {
+			throw new StockMarketExpection("Order does not exist in ordersPlaced");
+		}
 
-		o.getTrader().ordersPlaced.remove(o);
-		double cashMoved = o.getSize() * matchPrice;
-		//trader does not own any of this stock. adds new order to position
-		if (order == null && order instanceof BuyOrder) {
-			o.getTrader().position.add(o);
-			o.getTrader().cashInHand -= cashMoved;
+		if (SellOrder.class.isInstance(o)) {		
+			this.cashInHand += matchPrice * o.getSize(); // update cashInHand
+			OrderUtility.findAndExtractOrder(position, o.getStockSymbol()); // update position
+
+		} else if (BuyOrder.class.isInstance(o)) {
+			this.cashInHand -= matchPrice * o.getSize(); // update cashInHand
+			this.position.add(o); // update position
+
 		}
-		/*trader already owns some stock. adds to the order
-                in position correspondng to the stock*/
-		else if (order instanceof BuyOrder) {
-			order.setSize(order.getSize() + o.getSize());
-			o.getTrader().position.add(order);
-			o.getTrader().cashInHand -= cashMoved;
-		}
-		if (order == null && order instanceof SellOrder) {
-			throw new StockMarketExpection("Trader does not own any of this stock to sell");
-		}
-		/*trader has stock to sell. updates amount of stock trader owns*/
-		else if (order instanceof SellOrder){
-			order.setSize(order.getSize() - o.getSize());
-			o.getTrader().position.add(order);
-			o.getTrader().cashInHand += cashMoved;
-		}
+
+		this.ordersPlaced.remove(o); // update ordersPlaced
+
 	}
+
 
 	public void printTrader() {
 		System.out.println("Trader Name: " + name);
